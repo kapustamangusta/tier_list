aa5aa3ccc475df5419d6bfe4f857e1ae1fb0d544
Artem Sochinskiy <sochinskiyartom@gmail.com>
2024-09-20 14:01:41 +0300

    add app bar and scroll region for categories screen

--- lib/categories/view/categories_page.dart
+++ lib/categories/view/categories_page.dart
@@ -1,6 +1,7 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:super_drag_and_drop/super_drag_and_drop.dart';
 import 'package:tier_list_maker/categories/widgets/widgets.dart';
 import 'package:tier_list_maker/core/core.dart';
 
@@ -21,6 +22,39 @@ class _CategoriesPageState extends State<CategoriesPage> {
       'E',
       'F',
       'G',
+      'A',
+      'B',
+      'C',
+      'A',
+      'B',
+      'C',
+      'D',
+      'E',
+      'F',
+      'G',
+      'A',
+      'B',
+      'C',
+      'A',
+      'B',
+      'C',
+      'D',
+      'E',
+      'F',
+      'G',
+      'A',
+      'B',
+      'C',
+      'A',
+      'B',
+      'C',
+      'D',
+      'E',
+      'F',
+      'G',
+      'A',
+      'B',
+      'C',
     ],
     [
       '1',
@@ -30,6 +64,12 @@ class _CategoriesPageState extends State<CategoriesPage> {
       '5',
       '6',
       '7',
+      '1',
+      '2',
+      '3',
+      '4',
+      '5',
+      '6',
     ],
   ];
   final List<String> lower = [
@@ -50,6 +90,10 @@ class _CategoriesPageState extends State<CategoriesPage> {
   final int columns = 4;
   final int itemSpacing = 0;
   final double widthLeftPanel = 80;
+  final double heightLowerPanel = 120;
+  bool pinned = false;
+
+  final ScrollController _scrollController = ScrollController();
 
   void onDragStart(PanelLocation start) {
     String? data;
@@ -60,6 +104,7 @@ class _CategoriesPageState extends State<CategoriesPage> {
     }
 
     setState(() {
+      pinned = true;
       dragStart = start;
       hoveringData = data;
     });
@@ -87,92 +132,139 @@ class _CategoriesPageState extends State<CategoriesPage> {
       dragStart = null;
       dropPreview = null;
       hoveringData = null;
+      pinned = false;
     });
   }
 
   void updateDropPreview(PanelLocation update) => setState(() {
         dropPreview = update;
+
+        // _scrollController.jumpTo(_scrollController.offset - 10);
       });
   @override
   Widget build(BuildContext context) {
+    var theme = Theme.of(context);
     return Scaffold(
-      body: ReorderableListView(
-        proxyDecorator: (child, index, animation) {
-          return Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(child: child),
-          );
-        },
-        padding: const EdgeInsets.symmetric(horizontal: 16),
-        onReorder: (oldIndex, newIndex) {
-          setState(() {
-            if (oldIndex < newIndex) {
-              newIndex -= 1;
-            }
-            final List<String> item = upper.removeAt(oldIndex);
-            upper.insert(newIndex, item);
-          });
-        },
+      // appBar: AppBar(
+      //   title: const Text("Фильмы"),
+      //   actions: [
+      //     IconButton(onPressed: () {}, icon: const Icon(Icons.settings))
+      //   ],
+      // ),
+      body: Stack(
         children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 4),
-            key: Key(upper[0].join()),
-            child: CategoryCard(
-              color: const Color(0xFFFF7F7F),
-              columns: columns,
-              itemSpacing: itemSpacing,
-              widthLeftPanel: widthLeftPanel,
-              items: upper[0],
-              onDrop: drop,
-              onUpdateDropPreview: (p0) {
-                updateDropPreview(p0.copyWith(panelIndex: 0));
-              },
-              onDragStart: (p0) {
-                onDragStart(p0.copyWith(panelIndex: 0));
-              },
-              dragStart: dragStart?.$2 == Panel.upper && dragStart?.$3 == 0
-                  ? dragStart
-                  : null,
-              dropPreview:
-                  dropPreview?.$2 == Panel.upper && dropPreview?.$3 == 0
-                      ? dropPreview
-                      : null,
-              hoveringData:
-                  dropPreview?.$2 == Panel.upper && dropPreview?.$3 == 0
-                      ? hoveringData
-                      : null,
-            ),
+          CustomScrollView(
+            controller: _scrollController,
+            slivers: [
+              SliverAppBar(
+                floating: true,
+                pinned: false,
+                title: const Text("Фильмы"),
+                actions: [
+                  IconButton(onPressed: () {}, icon: const Icon(Icons.settings))
+                ],
+              ),
+              SliverReorderableList(
+                proxyDecorator: (child, index, animation) {
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Transform.scale(
+                      scale: 0.5,
+                      child: Container(child: child),
+                    ),
+                  );
+                },
+                onReorder: (oldIndex, newIndex) {
+                  setState(() {
+                    if (oldIndex < newIndex) {
+                      newIndex -= 1;
+                    }
+                    final List<String> item = upper.removeAt(oldIndex);
+                    upper.insert(newIndex, item);
+                  });
+                },
+                itemCount: upper.length,
+                itemBuilder: (context, index) =>
+                    ReorderableDelayedDragStartListener(
+                  key: Key(upper[index].join()),
+                  index: index,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 4,
+                      horizontal: 16,
+                    ),
+                    child: CategoryCard(
+                      color: const Color(0xFFFF7F7F),
+                      columns: columns,
+                      itemSpacing: itemSpacing,
+                      widthLeftPanel: widthLeftPanel,
+                      items: upper[index],
+                      onDrop: drop,
+                      onUpdateDropPreview: (p0) {
+                        updateDropPreview(p0.copyWith(panelIndex: index));
+                      },
+                      onDragStart: (p0) {
+                        onDragStart(p0.copyWith(panelIndex: index));
+                      },
+                      dragStart:
+                          dragStart?.$2 == Panel.upper && dragStart?.$3 == index
+                              ? dragStart
+                              : null,
+                      dropPreview: dropPreview?.$2 == Panel.upper &&
+                              dropPreview?.$3 == index
+                          ? dropPreview
+                          : null,
+                      hoveringData: dropPreview?.$2 == Panel.upper &&
+                              dropPreview?.$3 == index
+                          ? hoveringData
+                          : null,
+                    ),
+                  ),
+                ),
+              ),
+              SliverToBoxAdapter(
+                child: SizedBox(
+                  height: heightLowerPanel,
+                  width: MediaQuery.of(context).size.width,
+                ),
+              )
+            ],
           ),
-          Padding(
-            key: Key(upper[1].join()),
-            padding: const EdgeInsets.symmetric(vertical: 4),
-            child: CategoryCard(
-              key: Key(upper[1].join()),
-              color: const Color(0xFFFFBF7F),
-              columns: columns,
-              itemSpacing: itemSpacing,
-              widthLeftPanel: widthLeftPanel,
-              items: upper[1],
-              onDrop: drop,
-              onUpdateDropPreview: (p0) {
-                updateDropPreview(p0.copyWith(panelIndex: 1));
-              },
-              onDragStart: (p0) {
-                onDragStart(p0.copyWith(panelIndex: 1));
-              },
-              dragStart: dragStart?.$2 == Panel.upper && dragStart?.$3 == 1
-                  ? dragStart
-                  : null,
-              dropPreview:
-                  dropPreview?.$2 == Panel.upper && dropPreview?.$3 == 1
-                      ? dropPreview
-                      : null,
-              hoveringData:
-                  dropPreview?.$2 == Panel.upper && dropPreview?.$3 == 1
-                      ? hoveringData
-                      : null,
-            ),
-          ),
+          pinned
+              ? DropRegion(
+                  formats: Formats.standardFormats,
+                  onDropOver: (p0) async {
+                    _scrollController.jumpTo(_scrollController.offset - 5);
+
+                    return DropOperation.userCancelled;
+                  },
+                  onPerformDrop: (PerformDropEvent) async {},
+                  child: Container(
+                    color: Colors.black.withOpacity(0.2),
+                    height: 100,
+                    width: MediaQuery.of(context).size.width,
+                  ),
+                )
+              : const SizedBox(),
+          pinned
+              ? Positioned(
+                  bottom: 0,
+                  child: DropRegion(
+                    formats: Formats.standardFormats,
+                    onDropOver: (p0) async {
+                      _scrollController.jumpTo(_scrollController.offset + 5);
+
+                      return DropOperation.none;
+                    },
+                    onPerformDrop: (PerformDropEvent) async {},
+                    child: Container(
+                      color: Colors.black.withOpacity(0.2),
+                      height: 100,
+                      width: MediaQuery.of(context).size.width,
+                    ),
+                  ),
+                )
+              : const SizedBox(),
         ],
       ),
     );
--- lib/tier_list_app.dart
+++ lib/tier_list_app.dart
@@ -7,9 +7,24 @@ class TierListApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    var primaryColor = const Color(0xffFFB422);
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
+        primaryColor: primaryColor,
+        cardColor: Colors.white,
+        appBarTheme: AppBarTheme(
+          scrolledUnderElevation: 0.0,
+          centerTitle: true,
+          backgroundColor: Colors.white,
+          titleTextStyle: const TextStyle(
+            color: Colors.black,
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+          ),
+          actionsIconTheme: IconThemeData(color: primaryColor, size: 30),
+        ),
         scaffoldBackgroundColor: const Color(0xffEFF1F3),
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
